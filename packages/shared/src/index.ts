cdd252098bb3132526a940b2b9aa067a5151aa9c
Dillon Ring <dillon@dillonring.com>
2026-08-17 15:29:01 -0500

    Revert "Add untrusted value guards"
    This reverts commit 323caf1c6bc5ea7029934ccdefe7689331553948.

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,16 +1,5 @@
 export { apiRoutePatterns, apiRoutes, appRoutes } from './routes'
 export {
-	hasObjectType,
-	isBigInt,
-	isBoolean,
-	isFunction,
-	isNumber,
-	isString,
-	isSymbol,
-	isUndefined,
-} from './untrusted'
-export type { UntrustedInput } from './untrusted'
-export {
 	activeRecallGradeRequestSchema,
 	activeRecallGradeResponseSchema,
 	activeRecallModeSchema,
